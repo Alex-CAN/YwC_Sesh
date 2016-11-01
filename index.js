2039477011f8e08e4a2e647c7501687d7bea3c6e
john.h <jholman1993@gmail.com>
2016-11-01 16:37:37 -0700

    ex1 in the books, polish and more examples to come

---
+++ index.js
@@ -0,0 +1,14 @@
+var $btn = $('#request'),
+    $bio = $('#bio');
+
+$btn.on('click', function() {
+  $(this).hide();
+  $bio.load('./xzibitBio.txt', responseFunction);
+});
+
+function responseFunction(responseText, textStatus, request) {
+  $bio.css('border', '1px solid #e8e8e8');
+  if(textStatus == 'error') {
+      $bio.text('An error occurred during your request: ' +  request.status + ' ' + request.statusText);
+  }
+}
